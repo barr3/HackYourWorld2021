88d55d713b046ddde81d86dc203a1d5f8f35e033
Barre Blomberg <fred03.blomberg@gmail.com>
2021-02-19 23:53:34 +0100

    Created base "Template" for mobile application

--- IBMAss.py
+++ IBMAss.py
@@ -4,28 +4,28 @@ Kivy application test
 """
 import kivy
 from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.gridlayout import GridLayout
+from kivy.uix.button import Button
+from kivy.config import Config
+Config.set('graphics', 'width', '480')
+Config.set('graphics', 'height', '900')
+Config.set('graphics', 'resizable', False)
 from kivy.uix.textinput import TextInput
 
 
-
-class LoginScreen(GridLayout):
-
-    def __init__(self, **kwargs):
-        super(LoginScreen, self).__init__(**kwargs)
-        self.cols = 2
-        self.add_widget(Label(text='User Name'))
-        self.username = TextInput(multiline=False)
-        self.add_widget(self.username)
-        self.add_widget(Label(text='password'))
-        self.password = TextInput(password=True, multiline=False)
-        self.add_widget(self.password)
-
-class MyApp(App):
-
+class ButtonApp(App):
     def build(self):
-        return LoginScreen()
+        return Button()
+        
+    
+    def on_press_button(self):
+        print('You pressed the button!')
+#Experimental                   
+def on_enter(instance, value):
+    print('User pressed enter in', instance)
+
+    textinput = TextInput(text='Hello world', multiline=False)
+    textinput.bind(on_text_validate=on_enter)
 
 if __name__ == '__main__':
-    MyApp().run()
+    app = ButtonApp()
+    app.run()
