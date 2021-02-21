de3324dd9591cb51aa052a8fa297c90e520c6e0f
Barre Blomberg <fred03.blomberg@gmail.com>
2021-02-21 21:51:13 +0100

    Added ability to interact with onscreen label with textbar and button

--- KivyMDTest.py
+++ KivyMDTest.py
@@ -1,8 +1,29 @@
 from kivymd.app import MDApp
+from kivy.lang import Builder
+
+
 
 class MainApp(MDApp):
-    pass
+
+    def build(self):
+        self.theme_cls.primary_palette = "Blue"
+        self.theme_cls.theme_style = "Light"
+        self.theme_cls.primary_hue = "500"
+        self.screen = Builder.load_file("main.kv")
+        return self.screen
+        
+        
+    def press(self):
+        #print(self.screen.ids.input.text)
+        #self.screen.ids.response.text = "Snorre snorre bruh"
 
     
+    
+
+
+
+
+
 MainApp().run()
+
 
