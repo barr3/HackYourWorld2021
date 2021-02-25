30228671ef9703251b487c2122a1e97376739531
Barre <fred03.blomberg@gmail.com>
2021-02-25 10:32:12 +0100

    Removed old IBMAssistant code

--- KivyMDTest.py
+++ KivyMDTest.py
@@ -1,60 +1,11 @@
 from kivymd.app import MDApp
 from kivy.lang import Builder
 
-import json
-from ibm_watson import AssistantV2
-from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
 from kivy.config import Config
 Config.set('graphics', 'width', '370')
 Config.set('graphics', 'height', '600')
 Config.write()
-
-#Sets API Key
-authenticator = IAMAuthenticator('Paa0LICW2gszbzgDQX_83y5td9ggmRiv0H2RvA88uG0w')
-
-#Inits assistant
-assistant = AssistantV2(
-    version='2020-09-24',
-    authenticator=authenticator
-)
-assistant.set_service_url('https://api.eu-de.assistant.watson.cloud.ibm.com')
-
-sesh_id = assistant.create_session(
-        assistant_id='6bc3d805-70f6-4f71-9ee0-def44227ed12'
-    ).get_result()
-
-#Sets py_sesh_id to the session id
-py_sesh_id = sesh_id['session_id']
-
-
-#######################################
-
-#This function takes the input "input" and sends it to the assistant.
-#The function returns the response that the assistant gives. 
-def run_conversation(input):
-    user_input = input
-    
-    response = assistant.message(
-        assistant_id='6bc3d805-70f6-4f71-9ee0-def44227ed12',
-        session_id=py_sesh_id,
-        input={
-            'message_type': 'text',
-            'text': user_input
-        }
-    ).get_result()
-
-    #Really messy and ugly code. Could probaly be cleaned up but it works
-    output = response['output']
-    generic = output['generic']
-    generic_list = generic[0]
-    bot_output = generic_list['text']
-    #print(bot_output) 
-    return bot_output
-
-
-
-
 
 
 #This class runs the GUI, main.kv is where all the widgets are added
